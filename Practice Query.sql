8a69eecd432cd17a22e5c67d47456ae7af720354
Mr-r-a-d <141635915+Mr-r-a-d@users.noreply.github.com>
2023-08-11 10:35:37 +0530

    Add files via upload

---
+++ Practice Query.sql
@@ -0,0 +1,2 @@
+use office;
+desc info;
---
+++ lecture1.sql
@@ -0,0 +1,7 @@
+create database project;
+drop database work;
+show databases;
+use college;
+create table info(rollno int, name varchar(25), blood group varchar(3), phone number varchar (10), age int);
+create table info(rollno int, name varchar(25), bloodgroup varchar(3), phonenumber varchar (10), age int);
+desc info;
