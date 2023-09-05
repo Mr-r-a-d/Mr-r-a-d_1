3eef0ec00414f35cef2bd5254d06d87e1ef8b456
Mr-r-a-d <141635915+Mr-r-a-d@users.noreply.github.com>
2023-09-05 11:33:47 +0530

    Add files via upload

---
+++ cuboid.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int h,w;
+	printf("Enter h = ");
+	scanf("%d",&h);
+	printf("Enter w = ");
+	scanf("%d",&w);
+	float l;
+	printf("Enter l = ");
+	scanf("%f",&l);
+	int v=h*l*w
+	printf("Volume of cubeoid =%d ",v);
+	return 0;
+}
+
